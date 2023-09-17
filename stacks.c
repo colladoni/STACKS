2cc6df3e8f4c004f22d126ec284a4cc294314bd5
colladoni <61273055+colladoni@users.noreply.github.com>
2023-09-17 15:59:56 +0800

    reworked

--- stacks.c
+++ stacks.c
@@ -1,31 +1,8 @@
 #include <stdio.h>
-#include "stackADTarray.h"
-// #include "stackADTlinklist.h"
+// #include "stackADTarray.h"
+#include "stackADTlinklist.h"
 
-void insertBottomArr(STACK *S, char data){
-    STACK tempStack;
-    char elem;
 
-    initialize(&tempStack);
-
-    if (isFull(*S) || isEmpty(*S)) {
-        printf("Error inserting. \n");
-        return;
-    }
-    while (!isEmpty(*S)) {
-        elem = S->elem[S->top];
-        push(&tempStack, elem);
-        pop(S);
-    }
-
-    push(S,data);//insert bottom
-
-    while (!isEmpty(tempStack)) {
-        elem = tempStack.elem[tempStack.top];
-        push(S,elem);
-        pop(&tempStack);
-    }
-}
 
 int main(){
     STACK Ss;
@@ -49,10 +26,10 @@ int main(){
     printf("Stack after pushing D and E:\n");
     display(Ss);
 
-    insertBottomArr(&Ss,'J');
+    // insertBottom(&Ss,'J');
     
-    printf("Stack after inserting J in bottom:\n");
-    display(Ss);
+    // printf("Stack after inserting J in bottom:\n");
+    // display(Ss);
 
     return 0;
 }
