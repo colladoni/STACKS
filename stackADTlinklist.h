0d8d7cbea84b44d28f02f9b03d07099467d0e8ad
colladoni <61273055+colladoni@users.noreply.github.com>
2025-09-04 10:46:12 +0800

    Update stackADTlinklist.h

--- stackADTlinklist.h
+++ stackADTlinklist.h
@@ -1,77 +1,111 @@
-#ifndef STACKADTLINKLIST_H
-#define STACKADTLINKLIST_H
-#include <stdlib.h>
 #include <stdio.h>
+#define MAX 8
 
-typedef struct node{
-	char elem;
-	struct node *link;
-}*STACK;
+typedef struct{
+    char elem[MAX];
+    int front;
+    int rear;
+}QUEUE;
 
-void initialize(STACK *S);
-void push(STACK *S, char data);
-void pop(STACK *S);
-int isEmpty(STACK S);
-int isFull(STACK S);
-void display(STACK S);
-char top(STACK S);
+void initialize(QUEUE *Q);
+void EnQueue(QUEUE *Q, char data);
+void DeQueue(QUEUE *Q);
+int isFull(QUEUE Q);
+int isEmpty(QUEUE Q);
+char Front(QUEUE Q);
+void display(QUEUE Q);
 
+void insertBottom(QUEUE *Q, char data);
+int main(){
+    QUEUE Qq;
+    initialize(&Qq);
 
-void initialize(STACK *S){
-	S=NULL;
-}
+    EnQueue(&Qq, 'A');
+    EnQueue(&Qq, 'B');
+    EnQueue(&Qq, 'C');
 
-void push(STACK *S, char data){
-	STACK temp;
-	temp = (STACK)malloc(sizeof(struct node));
-	if (temp != NULL){
-	    temp->elem = data;
-	    temp->link = *S;
-	    *S = temp;
-	}
-}
+    printf("\nQueue after pushing A, B, C:\n");
+    display(Qq);
 
-void pop(STACK *S){
-    STACK temp;
-    if(!isEmpty(*S)){
-        temp = *S;
-        *S = temp->link;
-        free(temp);
-    }
-}
+    DeQueue(&Qq);
+    
 
-int isEmpty(STACK S){
-	return (S == NULL) ? 1:0;
-}
+    printf("\nQueue after popping one element:\n");
+    display(Qq);
 
-int isFull(STACK S){
+    EnQueue(&Qq, 'D');
+    EnQueue(&Qq, 'E');
+    
+    printf("\nQueue after pushing D and E:\n");
+    display(Qq);
+
+    insertBottom(&Qq,'J');
+    
+    printf("\nQueue after inserting J in bottom:\n");
+    display(Qq);
+
     return 0;
 }
 
-void display(STACK S){
-	STACK temp;
-	
-	if(isEmpty(S)){
-	    printf("Empty List");
-	}else{
-	    temp = NULL;
-	    
-        while(!isEmpty(S)){
-            push(&temp,top(S));
-            pop(&S);
-        }
-        while(!isEmpty(temp)){
-            push(&S,top(temp));
-            printf("%c",top(S));
-            pop(&temp);
-        }
+void initialize(QUEUE *Q){
+    Q->rear = 0;
+    Q->front = (Q->rear + 1) % MAX;
+}
+
+void EnQueue(QUEUE *Q, char data){
+    if(!isFull(*Q)){
+        Q->rear = (Q->rear + 1) % MAX;
+        Q->elem[Q->rear] = data;
     }
 }
 
-char top(STACK S){
-    if(!isEmpty(S)){
-        return S->elem;
+void DeQueue(QUEUE *Q){
+    if(!isEmpty(*Q)){
+        Q->front = (Q->front + 1) % MAX;
     }
 }
 
-#endif
+int isFull(QUEUE Q){
+    return (Q.front==(Q.rear + 2) % MAX) ? 1:0;
+}
+
+int isEmpty(QUEUE Q){
+    return (Q.front==(Q.rear + 1) % MAX) ? 1:0;
+}
+char Front(QUEUE Q){
+    char retval = '0';
+    if(!isEmpty(Q)){
+        retval = Q.elem[Q.front];
+    }
+    return retval;
+}
+void display (QUEUE Q){
+    int count = (((Q.rear - Q.front) + MAX) + 1) % MAX;
+    
+    for(;count>0;count--){
+        printf("%c",Front(Q));
+        EnQueue(&Q,(Front(Q)));
+        DeQueue(&Q);
+    }
+}
+
+void insertBottom(QUEUE *Q, char data){
+    QUEUE temp;
+
+    if(!isFull(*Q)&&!isEmpty(*Q)){
+	    initialize(&temp);
+        while(!isEmpty(*Q)){
+        	EnQueue(&temp, Front(*Q));
+		    DeQueue(Q);
+	    }
+    
+	    EnQueue(Q,data);
+
+	    while(!isEmpty(temp)){
+		    EnQueue(Q, Front(temp));
+		    DeQueue(&temp);
+        }
+    }else if(!isFull(*Q)&&isEmpty(*Q)){
+	EnQueue(Q,data);
+    }
+}
